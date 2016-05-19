ab7cf96f4098f62c62902d0bfa12345118293689
凌风 <yellow.716@163.com>
2016-05-19 18:54:58 +0800

    init blog

--- js/main.js
+++ js/main.js
@@ -90,3 +90,93 @@ setInterval(function(){
     changeBackground(0);
 
 },10000);
+
+
+
+/** 首页全屏处理 */
+
+//反射调用
+var invokeFieldOrMethod = function(element, method) {
+    var usablePrefixMethod; ["webkit", "moz", "ms", "o", ""].forEach(function(prefix) {
+        if (usablePrefixMethod) return;
+        if (prefix === "") {
+            // 无前缀，方法首字母小写
+            method = method.slice(0, 1).toLowerCase() + method.slice(1);
+        }
+        var typePrefixMethod = typeof element[prefix + method];
+        if (typePrefixMethod + "" !== "undefined") {
+            if (typePrefixMethod === "function") {
+                usablePrefixMethod = element[prefix + method]();
+            } else {
+                usablePrefixMethod = element[prefix + method];
+            }
+        }
+    });
+
+    return usablePrefixMethod;
+};
+
+//进入全屏
+function launchFullscreen(element) {
+
+    //此方法不可以在异步任务中执行，否则火狐无法全屏
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    } else if (element.oRequestFullscreen) {
+        element.oRequestFullscreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullScreen();
+    } else {
+
+        var docHtml = document.documentElement;
+        var docBody = document.body;
+        var videobox = document.getElementById('videobox');
+        var cssText = 'width:100%;height:100%;overflow:hidden;';
+        docHtml.style.cssText = cssText;
+        docBody.style.cssText = cssText;
+        videobox.style.cssText = cssText + ';' + 'margin:0px;padding:0px;';
+        document.IsFullScreen = true;
+
+    }
+}
+//退出全屏
+function exitFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.oRequestFullscreen) {
+        document.oCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else {
+        var docHtml = document.documentElement;
+        var docBody = document.body;
+        var videobox = document.getElementById('videobox');
+        docHtml.style.cssText = "";
+        docBody.style.cssText = "";
+        videobox.style.cssText = "";
+        document.IsFullScreen = false;
+    }
+}
+
+document.getElementById('fullScreenBtn').addEventListener('click',
+function() {
+    //document.documentElement;
+    launchFullscreen(document.getElementById('video'));
+    window.setTimeout(function exit() {
+
+        //检查浏览器是否处于全屏
+        if (invokeFieldOrMethod(document, 'FullScreen') || invokeFieldOrMethod(document, 'IsFullScreen') || document.IsFullScreen) {
+            exitFullscreen();
+        }
+    },
+    5 * 1000);
+},
+false);
